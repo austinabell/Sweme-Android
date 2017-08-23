265ad0f2a53262bc5f62b7bc0aff202067bf49fd
aabell8 <austinabell8@gmail.com>
2017-08-23 15:27:42 -0400

    Updated Swipe Library

--- app/src/main/java/austinabell8/sweme/MainActivity.java
+++ app/src/main/java/austinabell8/sweme/MainActivity.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
